d721f794fd6a0a3d51b03e13430267dc2341c557
Sam Snyder <sam@moderne.io>
2024-11-12 11:05:46 -0800

    Initial commit of tool which uses OpenRewrite to download the specified pom, for use in debugging

---
+++ build.gradle.kts
@@ -0,0 +1,33 @@
+plugins {
+    application
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.openrewrite:rewrite-maven:latest.release")
+}
+
+testing {
+    suites {
+        // Configure the built-in test suite
+        val test by getting(JvmTestSuite::class) {
+            // Use JUnit Jupiter test framework
+            useJUnitJupiter("5.10.3")
+        }
+    }
+}
+
+// Apply a specific Java toolchain to ease working on different environments.
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(17)
+    }
+}
+
+application {
+    // Define the main class for the application.
+    mainClass = "org.example.App"
+}
